7a237f9e8bb60e3364fa29cab05d9f8524cc1f5e
KuroBlack <blackjiackherrera@gmail.com>
2023-05-13 18:22:52 -0500

    cambios agregados a otro de los archivos, faltan algunos otros para cer mejorados

--- Curso profesional de C++/Calculadora-de-matrices-sumas-y-restas..cpp
+++ Curso profesional de C++/Calculadora-de-matrices-sumas-y-restas..cpp
@@ -6,42 +6,48 @@
 
 using namespace std;
 
+void clearScreen();
+
+
 int Matriz1[10][10], Matriz2[10][10],MatrizResultado[10][10];
 int fla1, Clun1, fla2, Clun2;
-int main()
-{
+int main() {
 	char opc;
 	do {
-	    system("cls");
-	cout << "Ingrese Cantidad De Filas de la primera matriz: ";
-	cin >> fla1;
-	cout << "Ingrese Cantidad De Columna de la primera matriz: ";
-	cin >> Clun1;
-	cout << endl;
-	cout << "Ingrese Cantidad De Filas de la segunda matriz: ";
-	cin >> fla2;
-	cout << "Ingrese Cantidad De Columna de la segunda matriz: ";
-	cin >> Clun2;
-	cout << "\nIngrese Dato de La Primera matriz" << endl;
-	for (int i = 0; i < fla1; i++) {
-		for (int j = 0; j < Clun1; j++) {
-			cout << "Ingrese Dato [" << i << "][" << j << "] :";
-			cin >> Matriz1[i][j];
+
+		cout << "Ingrese Cantidad De Filas de la primera matriz: ";
+		cin >> fla1;
+		cout << "Ingrese Cantidad De Columna de la primera matriz: ";
+		cin >> Clun1;
+		cout << endl;
+		clearScreen();
+		cout << "Ingrese Cantidad De Filas de la segunda matriz: ";
+		cin >> fla2;
+		cout << "Ingrese Cantidad De Columna de la segunda matriz: ";
+		cin >> Clun2;
+		clearScreen();
+		cout << "\nIngrese Dato de La Primera matriz" << endl;
+		for (int i = 0; i < fla1; i++) {
+			for (int j = 0; j < Clun1; j++) {
+				cout << "Ingrese Dato [" << i << "][" << j << "] :";
+				cin >> Matriz1[i][j];
+			}
 		}
-	}
-	cout << "\nIngrese Dato de La Segunda matriz" << endl;
-	for (int i = 0; i < fla2; i++) {
-		for (int j = 0; j < Clun2; j++) {
-			cout << "Ingrese Dato [" << i << "][" << j << "] :";
-			cin >> Matriz2[i][j];
+		clearScreen();
+		cout << "\nIngrese Dato de La Segunda matriz" << endl;
+		for (int i = 0; i < fla2; i++) {
+			for (int j = 0; j < Clun2; j++) {
+				cout << "Ingrese Dato [" << i << "][" << j << "] :";
+				cin >> Matriz2[i][j];
+			}
 		}
-	}
+		clearScreen();
 		int SmRes;
 		cout << "1. Sumar: " << endl;
 		cout << "2. Restar: " << endl;
 		cout << "Seleccione: ";
 		cin >> SmRes;
-
+		clearScreen();
 		if (SmRes == 1) {
 			if ((fla1 == fla2)&&(Clun1 == Clun2)) {
 				for (int i = 0; i < fla2; i++) {
@@ -85,5 +91,10 @@ int main()
 		cout<<" C. Vover Al Principio: "<<endl;
 		cout<<" Seleccionar: ";
 		cin>>opc;
+		clearScreen();
 	} while ((opc != 'S' || opc != 's') && (opc == 'C' || opc == 'c'));
+}
+
+void clearScreen() {
+	cout << "\033[2J\033[1;1H";
 }
